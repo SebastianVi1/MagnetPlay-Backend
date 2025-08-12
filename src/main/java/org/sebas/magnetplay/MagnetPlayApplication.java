114fc0530895139008f6d3cee20435a63e593561
Andre <sebastianvh86@gmail.com>
2025-08-11 18:11:57 -0600

    First commit

---
+++ src/main/java/org/sebas/magnetplay/MagnetPlayApplication.java
@@ -0,0 +1,13 @@
+package org.sebas.magnetplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnetPlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagnetPlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sebas/magnetplay/MagnetPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sebas.magnetplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnetPlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
