0967a187d2dccf6458c92d595019838488c2e8f4
Andre <sebastianvh86@gmail.com>
2025-08-17 23:00:19 -0600

    Add CORS configuration to allow requests frontend

---
+++ src/main/java/org/sebas/magnetplay/controller/WebConfig.java
@@ -0,0 +1,24 @@
+package org.sebas.magnetplay.controller;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173") // uri to allow
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") //operations to allow
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
